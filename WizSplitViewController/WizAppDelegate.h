04d0db4f7001476827e6697459f0e10f45d3d525
dzpqzb <dz@wiz.cn>
2013-03-21 10:05:01 +0800

    Initial Commit

---
+++ WizSplitViewController/WizAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WizAppDelegate.h
+//  WizSplitViewController
+//
+//  Created by dzpqzb on 13-3-21.
+//  Copyright (c) 2013年 wiz.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WizAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
